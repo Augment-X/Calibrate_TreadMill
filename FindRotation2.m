93276d2b704a2cf162129bb94e901d1e7810e14c
TurcksinTom <AugmentXatvub@gmail.com>
2025-06-03 13:09:28 +0200

    Optimization Loop Works

--- FindRotation2.m
+++ FindRotation2.m
@@ -1,56 +1,4 @@
-clc; clear; close; 
-
-% Define parmeters
-X0 = [0; 0; 0];       % Innitial guess rotation (to be entered into Vicon) 
-%X0 = X0 *pi/180;
-
-%%
-
-%%%%%% INNER Script TESTING %%%%%%%
-
-global MakersFromVicon;
-
-% % MakersFromVicon= ...
-% %   [-234.3254, -249.6393,  253.8878; ...
-% %  -190.0241, 199.7790,  -195.2969 ; ...
-% %    107.6214 ,  19.1969 , -0.1742] ;
-% 
-% % MakersFromVicon = ...                 
-% %    [ 266.6853, -114.2799 , 114.2799;...
-% %  -141, -299.0654,  299.0654;...
-% %    10.0000 ,  10.0000  , 10.0000] ;
-% % rot = [0; 0; 60]; 
-%
-  MakersFromVicon= ...
-[ -229.6853, -259.2799 , 262.2799;...
- -41.1411, -116.0654,  -97.0654;...
-   219.0000 ,  -146.0000  , 154.0000] ;
-rot = [0; 10; 90]; 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Value from Nexus measurment [Marker1(top left), Marker2(top right), Marker3(bottom left)]
-
-vicon = ViconNexus();
-Markers = vicon.GetMarkerNames('ForcePlate3Points');
-n = length(Markers);
-
-trajX = cell(1, n); trajY = cell(1, n); trajZ = cell(1, n);
-for i = 1:n
- [trajX{i}, trajY{i}, trajZ{i}, ~] = vicon.GetTrajectory('ForcePlate3Points', Markers{i});
-end
-X = cellfun(@mean, trajX);
-Y = cellfun(@mean, trajY);
-Z = cellfun(@mean, trajZ);
-
-M1 = [X(1); Y(1); Z(1)];
-M2 = [X(2); Y(2); Z(2)];
-M3 = [X(3); Y(3); Z(3)];
-
-Center = (M2+M3)/2;
-
-%global MakersFromVicon;
-%MakersFromVicon =[M1-Center,M2-Center,M3-Center];
+clc; clear all; close all; 
 
 %% DEFINE FORCE PLATE OBJECT        
 FPLenght = 600; % mm
@@ -58,98 +6,129 @@ FPwidth = 500; % mm
 edgeBand = 50; % Width death zone mm (for marker position) 
 UnitVecFactor = 100; % Scaling Factor (mm) of unitary Frame of Reference
 MarkerHeight = 10; % Marker height base to centroid 
-theta=pi;
-ROT_init=[cos(theta) -sin(theta) 0;...
-          sin(theta) cos(theta) 0;...
-          0 0 1];
 
-MarkersVec_1=[
-    -FPwidth/2+edgeBand, FPwidth/2-edgeBand, -FPwidth/2+edgeBand;...
-    FPLenght/2-edgeBand, FPLenght/2-edgeBand, -FPLenght/2+edgeBand;...
-    MarkerHeight, MarkerHeight, MarkerHeight];
+global MarkersVec EdgeVec unitVecX unitVecY unitVecZ MakersFromViconRotCalc MakersFromVicon;
 
-global MarkersVec
-MarkersVec = ROT_init*MarkersVec_1;
 EdgeVec = ...
     [FPwidth/2, -FPwidth/2 , -FPwidth/2,  FPwidth/2, FPwidth/2;...
     FPLenght/2, FPLenght/2, -FPLenght/2, -FPLenght/2,  FPLenght/2;...
     0, 0 , 0 , 0 , 0];
 
-% global MarkersVec
-% MarkersVec = ...
-%     [-FPwidth/2+edgeBand, FPwidth/2-edgeBand , -FPwidth/2+edgeBand;...
-%     FPLenght/2-edgeBand, FPLenght/2-edgeBand, -FPLenght/2+edgeBand;...
-%     MarkerHeight, MarkerHeight , MarkerHeight];
+ MarkersVec = ...
+     [-FPwidth/2+edgeBand, FPwidth/2-edgeBand , -FPwidth/2+edgeBand;...
+     FPLenght/2-edgeBand, FPLenght/2-edgeBand, -FPLenght/2+edgeBand;...
+     MarkerHeight, MarkerHeight , MarkerHeight];
 
 unitVecX= [0, UnitVecFactor; 0 , 0 ; 0 , 0]; 
 unitVecY= [0, 0; 0 , UnitVecFactor ; 0 , 0]; 
 unitVecZ= [0, 0; 0 , 0 ; 0 , UnitVecFactor]; 
 
+%% VICON MEASUREMENT DATA
 
+%%%%%% Useing the algorithm with Vicon Workspace %%%%%%%
+% % Value from Nexus measurment [Marker1(top left), Marker2(top right), Marker3(bottom left)]
+% % Connect to Vicon Nexus
+% vicon = ViconNexus();
+% Markers = vicon.GetMarkerNames('ForcePlate3Points');
+% n = length(Markers);
 
-%% VISUALIZE %% initial position 
-figure(); hold on; axis equal; grid on;  xlim([-1000, 1000]); ylim([-1000, 1000])
+% trajX = cell(1, n); trajY = cell(1, n); trajZ = cell(1, n);
+% for i = 1:n
+%  [trajX{i}, trajY{i}, trajZ{i}, ~] = vicon.GetTrajectory('ForcePlate3Points', Markers{i});
+% end
+% X = cellfun(@mean, trajX);
+% Y = cellfun(@mean, trajY);
+% Z = cellfun(@mean, trajZ);
+
+% M1 = [X(1); Y(1); Z(1)];
+% M2 = [X(2); Y(2); Z(2)];
+% M3 = [X(3); Y(3); Z(3)];
+
+% Center = (M2+M3)/2;
+% disp('position of Force Plate Center:');
+% disp(Center);
+% MakersFromViconRotCalc =[M1-Center,M2-Center,M3-Center];
+
+%%%%%% INNER Script TESTING %%%%%%%
+% We create fake measurement data to simulate the Vicon output to help develop the algorithm. 
+% Uncomment the following lines to use the hardcoded values for testing
+
+testRot = [-80; 15; -85]; 
+testRot = testRot*pi/180; % Rotation to be applied to markers from vicon (in radians)
+testROT = rotationMatrix(testRot); % build test rotation matrix
+MakersFromViconRotCalc = testROT * MarkersVec; % apply rotation to markers
+
+%% VISUALIZE %% initial position
+figure(1); hold on; axis equal; grid on;  xlim([-1000, 1000]); ylim([-1000, 1000])
 
 plot3(EdgeVec(1,:), EdgeVec(2,:), EdgeVec(3,:),':k')
 plot3(MarkersVec(1,:), MarkersVec(2,:), MarkersVec(3,:),'*k')
 plot3(unitVecX(1,:), unitVecX(2,:), unitVecX(3,:),':r')
 plot3(unitVecY(1,:), unitVecY(2,:), unitVecY(3,:),':g')
 plot3(unitVecZ(1,:), unitVecZ(2,:), unitVecZ(3,:),':b')
-plot3(MakersFromVicon(1,:), MakersFromVicon(2,:), MakersFromVicon(3,:),'ok')
+plot3(MakersFromViconRotCalc(1,:), MakersFromViconRotCalc(2,:), MakersFromViconRotCalc (3,:),'ok')
 
 
 %% OPTIMIZATION
 
-Xub = [180; 180; 180]; Xlb = -Xub; % Upper and lower bounds
-OPTIONS = optimoptions('fmincon','Algorithm','SQP');  % alternatives : interior point, SQP,  active-set, and trust-region-reflective
+global err 
+err = [100];
+Xub = [pi; pi; pi]; Xlb = -Xub; % Upper and lower bounds
+X0 = [0; 0; 0];      % Innitial seed rotation 
 
-Xopt = fmincon(@objective, X0, [], [], [], [], Xlb, Xub, [], OPTIONS );
+while err(end) >=5
+    OPTIONS = optimoptions('fmincon','Algorithm','SQP');  % alternatives : interior point, SQP,  active-set, and trust-region-reflective
+    Xopt = fmincon(@objective, X0, [], [], [], [], Xlb, Xub, [], OPTIONS );  
+    X0 = randn(3,1)*pi; % New seed for next iteration
+end
 
-% FINAL ANSWER %%
-disp(Xopt);
 
-%% Rotate object 
+% Output to terminal
+disp(Xopt*180/pi); % FINAL ANSWER (to be entered into Vicon) 
+disp('instead of :');
+disp(testRot*180/pi); % TEST ROTATION (to be compared with Vicon)
+disp('Markers from rotation calculation is:');
+disp(MakersFromViconRotCalc);
+
+%% Rotate object after optimization
 
 ROT = rotationMatrix(Xopt);
-
 % Transform Object 
 EdgeVec = ROT*EdgeVec;
-MarkersVec = ROT*MarkersVec;
+MarkersVecFinal = ROT*MarkersVec;
 unitVecX = ROT*unitVecX;
 unitVecY= ROT*unitVecY;
 unitVecZ = ROT*unitVecZ;
 
 
 %% VISUALIZE %% final position
-%figure(); hold on; axis equal; grid on;  
+figure(1); %hold on; axis equal; grid on;  
 
 plot3(EdgeVec(1,:), EdgeVec(2,:), EdgeVec(3,:),'-k')
-plot3(MarkersVec(1,:), MarkersVec(2,:), MarkersVec(3,:),'*k')
+plot3(MarkersVecFinal(1,:), MarkersVecFinal(2,:), MarkersVecFinal(3,:),'*k')
 plot3(unitVecX(1,:), unitVecX(2,:), unitVecX(3,:),'-r')
 plot3(unitVecY(1,:), unitVecY(2,:), unitVecY(3,:),'-g')
 plot3(unitVecZ(1,:), unitVecZ(2,:), unitVecZ(3,:),'-b')
 
+figure(2); hold on; grid on;  
+plot(err); ylabel('Objective Function Value (mm)');
 
-%% OPTIMIZATION 
+%% OPTIMIZATION
 
 function obj = objective(X1)
-    
     ROT1 = rotationMatrix(X1);
-    
-    global MarkersVec 
-    global MakersFromVicon
-    
+    global MarkersVec  
+    global MakersFromViconRotCalc
+    global err
     MarkersVec0 = ROT1*MarkersVec;
-    
-    obj = sum(sum((MarkersVec0-MakersFromVicon).^2));
-
+    obj = sqrt(sum(sum((MarkersVec0-MakersFromViconRotCalc).^2)));
+    err = [err, obj];
 end
 
 
-function ROT2 = rotationMatrix(X)
-
-    X2=deg2rad(X);
-    steps = 1; 
+function ROT2 = rotationMatrix(X2)
+   % X2=deg2rad(X);
+    steps = 1;             % X2 is divided into steps to approximate the rotation X2 is in Radians
     X2 = X2/steps;
     ROT2 = eye(3);
     for i = 1 : steps
@@ -158,5 +137,4 @@ function ROT2 = rotationMatrix(X)
         rotZ = [cos(X2(3)), -sin(X2(3)),0 ; sin(X2(3)), cos(X2(3)), 0 ; 0, 0, 1 ]; 
         ROT2 = rotZ*rotY*rotX*ROT2;
     end
-    %size(ROT2)
 end
